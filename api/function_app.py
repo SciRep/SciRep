2b325abafe2743b4af1d30b2fb671591d7ba62f6
SciRep <108672712+SciRep@users.noreply.github.com>
2024-08-07 20:04:34 -0400

    Delete api directory

--- api/function_app.py
+++
@@ -1,6 +0,0 @@
-import azure.functions as func
-import logging
-from app import app
-
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
